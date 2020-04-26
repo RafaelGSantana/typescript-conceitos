fffae9d94c7a2803cb89c6cf976be5c8d680402a
Rafael Santana <rgs1990419@gmail.com>
2020-04-26 14:09:56 -0300

    Criando projeto conceito

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import createUser from "./services/createUser";
+
+export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: "rgs@gmail.com",
+    password: "123456",
+    techs: [
+      "Node.js",
+      "ReactJS",
+      "React Native",
+      { title: "Javascript", experience: 100 },
+    ],
+  });
+
+  return response.json({ message: "Hello GoStack" });
+}
